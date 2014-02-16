4c6b4d41749cc280f8bf8bd4f6ed41059a1e464c
Henrik Stiernstedt <henrikstiernstedt@Henriks-MacBook-Pro.local>
2014-02-16 13:18:44 +0100

    Testcommit
    Test of a regular commit.

--- webwar.js
+++ webwar.js
@@ -11,12 +11,31 @@ App = function()
 
 	this.init = function()
 	{
-		var sprite = new Sprite('assets/mole.png');
+		/*
+		var sprite = new Sprite('assets/mole.png',10);
 		var mole = new SceneObject(sprite);
 		wade.addSceneObject(mole);
+		*/
+        this.createMainMenu();
 		
-		var grid = new Sprite();
-		wade.addSceneObject(new SceneObject(grid));
-		grid.setDrawFunction(wade.drawFunctions.grid_(20,20,'white'));
 	};
-};
+	
+	
+	this.createMainMenu = function()
+	{
+		gameState = 'mainMenu';
+	    var screenWidth = wade.getScreenWidth();
+	    var screenHeight = wade.getScreenHeight();
+	    wade.setMinScreenSize(0, 0);
+	    
+        // create background gradient
+        var backSprite = new Sprite(null, 20);
+        backSprite.setSize(screenWidth, screenHeight);
+        backSprite.setDrawFunction(wade.drawFunctions.gradientFill_({x: 0, y: 1}, ['#444', '#000']));
+        var backObject = new SceneObject(backSprite);
+        wade.addSceneObject(backObject);
+	};
+
+	
+	
+}
